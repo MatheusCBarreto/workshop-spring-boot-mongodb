d22190b6377ee04a7bdd7e5a3423a70025a71eee
MatheusCBarreto <devmattbarreto@protonmail.com>
2024-05-23 15:58:25 -0300

    first commit

---
+++ src/main/java/com/matheusbarreto/workshopmongodb/WorkshopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.matheusbarreto.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusbarreto/workshopmongodb/WorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusbarreto.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
