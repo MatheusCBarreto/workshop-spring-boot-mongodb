3e33d6c83513d83a8a6a7e610d02c85a7aa13523
MatheusCBarreto <devmattbarreto@protonmail.com>
2024-05-29 15:30:04 -0300

    Consulta simples com @Query

--- src/main/java/com/matheusbarreto/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/matheusbarreto/workshopmongodb/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.matheusbarreto.workshopmongodb.repository;
 
 import com.matheusbarreto.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-    List<Post> findByTitleContainingIgnoreCase(String title);
+    @Query("{ 'title' : { $regex: ?0 , $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/matheusbarreto/workshopmongodb/services/PostService.java
+++ src/main/java/com/matheusbarreto/workshopmongodb/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String title) {
-        return repository.findByTitleContainingIgnoreCase(title);
+        return repository.searchTitle(title);
     }
 }
